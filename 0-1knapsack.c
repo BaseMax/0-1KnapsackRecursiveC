979f9baeaea4e7a52d2e7b47158163883424d33f
Max Base <MaxBaseCode@Gmail.Com>
2023-01-14 14:47:23 +0100

    Create 0-1knapsack.c

---
+++ 0-1knapsack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int knapSackRecursive(int W, int wt[], int val[], int n)
+{
+    if (n == 0 || W == 0) {
+        return 0;
+    }
+    if (wt[n-1] > W) {
+        return knapSackRecursive(W, wt, val, n-1);
+    } else {
+        return MAX(val[n-1] + knapSackRecursive(W-wt[n-1], wt, val, n-1), knapSackRecursive(W, wt, val, n-1));
+    }
+}
