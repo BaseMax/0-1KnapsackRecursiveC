25faefb972e30dff545ae8d590aa534cbe9cf3db
Max Base <MaxBaseCode@Gmail.Com>
2023-01-14 14:48:03 +0100

    Update 0-1knapsack.c

--- 0-1knapsack.c
+++ 0-1knapsack.c
@@ -5,12 +5,10 @@
 
 int knapSackRecursive(int W, int wt[], int val[], int n)
 {
-    if (n == 0 || W == 0) {
-        return 0;
-    }
-    if (wt[n-1] > W) {
-        return knapSackRecursive(W, wt, val, n-1);
-    } else {
-        return MAX(val[n-1] + knapSackRecursive(W-wt[n-1], wt, val, n-1), knapSackRecursive(W, wt, val, n-1));
-    }
+    if (n == 0 || W == 0) return 0;
+    else if (wt[n-1] > W) return knapSackRecursive(W, wt, val, n-1);
+    else return MAX(
+        val[n-1] + knapSackRecursive(W-wt[n-1], wt, val, n-1),
+        knapSackRecursive(W, wt, val, n-1)
+    );
 }
