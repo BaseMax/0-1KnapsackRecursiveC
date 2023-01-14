ba3149ddbfb4efe79270e3435024b532cc5aad3b
Max Base <MaxBaseCode@Gmail.Com>
2023-01-14 14:46:52 +0100

    Create example.c

---
+++ example.c
@@ -0,0 +1,14 @@
+#include "0-1knapsack.c"
+
+int main()
+{
+    int W = 50;
+    int wt[] = {10, 20, 30};
+    int val[] = {60, 100, 120};
+    int n = sizeof(wt)/sizeof(wt[0]);
+
+    int max_value = knapSackRecursive(W, wt, val, n);
+    printf("Maximum value that can be put in knapsack: %d\n", max_value);
+
+    return 0;
+}
